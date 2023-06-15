e6552dd98f9071b493587112f21b7fd196202718
Edannan <emmanueldansoannan@gmail.com>
2023-06-15 08:13:47 +0000

    working on the next button bug

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -45,29 +45,61 @@ for (var i = 0; i < thumbnails.length; i++) {
   }
 
 
-  //switching images at the lightbox
-  var modalThumbnails = document.getElementsByClassName("thumbnail-modal");
-  var modalImage = document.getElementById("modalImage");
+  //switching main image at the lightbox
+function switchModalImg(){
+var modalThumbnails = document.getElementsByClassName("thumbnail-modal");
+var modalImage = document.getElementById("modalImage");
+var currentImg = document.getElementsByClassName("activeModImg");
+for (var i = 0; i < modalThumbnails.length; i++) {
+  if(currentImg[0] == modalThumbnails[0]){
+    modalImage.style.backgroundImage = "url('images/image-product-1.jpg')";
+  }else if(currentImg[0] == modalThumbnails[1]){
+    modalImage.style.backgroundImage = "url('images/image-product-2.jpg')";
+  }else if(currentImg[0] == modalThumbnails[2]){
+    modalImage.style.backgroundImage = "url('images/image-product-3.jpg')";
+  }else{
+    modalImage.style.backgroundImage = "url('images/image-product-4.jpg')";
+  }
+}
+}
 
+//switching active Lightbox ThumbnailsImage
+var modalThumbnails = document.getElementsByClassName("thumbnail-modal");
+var modalImage = document.getElementById("modalImage");
 for (var i = 0; i < modalThumbnails.length; i++) {
   modalThumbnails[i].addEventListener("click", function() {
       var currentImg = document.getElementsByClassName("activeModImg");
       currentImg[0].className = currentImg[0].className.replace("activeModImg", "");
       this.classList.add("activeModImg");
-      
-      if(currentImg[0] == modalThumbnails[0]){
-        modalImage.style.backgroundImage = "url('images/image-product-1.jpg')";
-      }else if(currentImg[0] == modalThumbnails[1]){
-        modalImage.style.backgroundImage = "url('images/image-product-2.jpg')";
-      }else if(currentImg[0] == modalThumbnails[2]){
-        modalImage.style.backgroundImage = "url('images/image-product-3.jpg')";
-      }else{
-        modalImage.style.backgroundImage = "url('images/image-product-4.jpg')";
-      }
+
+      switchModalImg();
     });
   }
 
+  //previous button
   function prev(){
     var modalThumbnails = document.getElementsByClassName("thumbnail-modal");
     var currentImg = document.getElementsByClassName("activeModImg");
+
+    for(var i = 0; i < modalThumbnails.length; i++){
+      if(modalThumbnails[i].classList.contains("activeModImg") && modalThumbnails[i] != modalThumbnails[0]){
+        modalThumbnails[i].classList.remove("activeModImg");
+        modalThumbnails[i-1].classList.add("activeModImg");
+      }
+    }
+    switchModalImg();
+  }
+
+  //fixing error here
+  function next(){
+    var modalThumbnails = document.getElementsByClassName("thumbnail-modal");
+    var currentImg = document.getElementsByClassName("activeModImg");
+    for(var i = 0; i < modalThumbnails.length; i++){
+      if(modalThumbnails[i].classList.contains("activeModImg") && modalThumbnails[i] != (modalThumbnails[3])){
+        modalThumbnails[i].classList.remove("activeModImg");
+        modalThumbnails[i+1].classList.add("activeModImg");
+        console.log(modalThumbnails[i+1]);
+      }
+    }
+    switchModalImg();
   }
